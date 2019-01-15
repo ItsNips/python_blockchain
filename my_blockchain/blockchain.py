166f5320641e6babe1413c9cc55010b5e81c598f
Kazak Aliaksei <a.kazak@pixelplex.io>
2019-01-15 18:30:08 +0300

    add more complex 'add transaction'

--- my_blockchain/blockchain.py
+++ my_blockchain/blockchain.py
@@ -1,5 +1,10 @@
 # Initializing our (empty) blockchain list
-blockchain = []
+genesis_block = {
+    "previous_hash": "",
+    "index": 0,
+    "transactions": []
+}
+blockchain = [genesis_block]
 open_transactions = []
 owner = "Aliaksei"
 
@@ -31,7 +36,18 @@ def add_transaction(tx_recipient, tx_sender=owner, tx_amount=1.0):
 
 
 def mine_block():
-    pass
+    last_block = blockchain[-1]
+    hashed_block = ""
+    for key in last_block:
+        value = last_block[key]
+        hashed_block = hashed_block + str(value)
+
+    block = {
+        "previous_hash": hashed_block,
+        "index": len(blockchain),
+        "transactions": open_transactions
+    }
+    blockchain.append(block)
 
 
 def get_transaction_value():
@@ -83,7 +99,8 @@ waiting_for_input = True
 while waiting_for_input:
     print("Please choose:")
     print("1: Add a new transaction value")
-    print("2: Output the blockchain blocks")
+    print("2: Mine a new block")
+    print("3: Output the blockchain blocks")
     print("h: Manipulate the chain")
     print("q: Quit")
     user_choice = get_user_choice()
@@ -93,6 +110,8 @@ while waiting_for_input:
         # Add the transaction amount to the blockchain
         add_transaction(recipient, tx_amount=amount)
     elif "2" == user_choice:
+        mine_block()
+    elif "3" == user_choice:
         print_blockchain_elements()
     elif "h" == user_choice:
         # Make sure that you don't try to "hack" the blockchain if it's empty
@@ -103,11 +122,11 @@ while waiting_for_input:
         waiting_for_input = False
     else:
         print("Input was invalid, please pick a value from the list!")
-    if not verify_chain():
-        print("Invalid blockchain!")
-        print_blockchain_elements()
-        # Break out of the loo
-        break
+    # if not verify_chain():
+    #     print("Invalid blockchain!")
+    #     print_blockchain_elements()
+    #     # Break out of the loo
+    #     break
 else:
     print("User left!")
 
