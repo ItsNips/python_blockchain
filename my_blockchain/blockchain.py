7f7c1896e863b79e15d3b5c3bbe67851897d02ce
Kazak Aliaksei <a.kazak@pixelplex.io>
2019-01-15 17:10:02 +0300

    some refactoring

--- my_blockchain/blockchain.py
+++ my_blockchain/blockchain.py
@@ -43,25 +43,26 @@ def print_blockchain_elements():
     for block in blockchain:
         print("Outputting Block")
         print(block)
+    else:
+        print("-" * 20)
 
 
 def verify_chain():
-    block_index = 0
     is_valid = True
-    for block in blockchain:
+    for block_index in range(len(blockchain)):
         if block_index == 0:
-            block_index += 1
             continue
-        if block[0] == blockchain[block_index - 1]:
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
             is_valid = True
         else:
             is_valid = False
             break
-        block_index += 1
     return is_valid
 
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
     print("Please choose:")
     print("1: Add a new transaction value")
     print("2: Output the blockchain blocks")
@@ -77,11 +78,14 @@ while True:
         if len(blockchain) >= 1:
             blockchain[0] = [2]
     elif "q" == user_choice:
-        break
+        waiting_for_input = False
     else:
         print("Input was invalid, please pick a value from the list!")
     if not verify_chain():
         print("Invalid blockchain!")
+        print_blockchain_elements()
         break
+else:
+    print("User left!")
 
 print("Done!")
