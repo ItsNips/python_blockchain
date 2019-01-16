4e565349789eb2dca604a3cad2435c57337970b0
Kazak Aliaksei <a.kazak@pixelplex.io>
2019-01-16 10:59:14 +0300

    add participants

--- my_blockchain/blockchain.py
+++ my_blockchain/blockchain.py
@@ -7,6 +7,7 @@ genesis_block = {
 blockchain = [genesis_block]
 open_transactions = []
 owner = "Aliaksei"
+participants = {"Aliaksei"}
 
 
 def hash_block(block):
@@ -37,6 +38,8 @@ def add_transaction(tx_recipient, tx_sender=owner, tx_amount=1.0):
         "amount": tx_amount
     }
     open_transactions.append(transaction)
+    participants.add(tx_sender)
+    participants.add(tx_recipient)
 
 
 def mine_block():
@@ -95,6 +98,7 @@ while waiting_for_input:
     print("1: Add a new transaction value")
     print("2: Mine a new block")
     print("3: Output the blockchain blocks")
+    print("4: Output participants")
     print("h: Manipulate the chain")
     print("q: Quit")
     user_choice = get_user_choice()
@@ -107,6 +111,8 @@ while waiting_for_input:
         mine_block()
     elif "3" == user_choice:
         print_blockchain_elements()
+    elif "4" == user_choice:
+        print(participants)
     elif "h" == user_choice:
         # Make sure that you don't try to "hack" the blockchain if it's empty
         if len(blockchain) >= 1:
